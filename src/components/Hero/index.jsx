801d54452d79836f658e0b76712833966bfa436d
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2023-12-20 09:20:46 +0100

    Removed "prettier-plugin-tailwindcss"

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     return (
         <section className="bg-gradient-to-b from-custom-400 to-custom-300">
             <div className="u-container flex min-h-screen flex-col">
-                <div className="lg:u-grid flex-grow lg:grid-cols-2 flex flex-col">
+                <div className="lg:u-grid lg:grid-cols-2 grid grid-rows-2 lg:grid-rows-none flex-grow">
                     <div className="flex flex-col justify-center gap-s-fl-xl flex-grow">
                         <p className="origin-top-left -rotate-6 font-gloriaHallelujah text-t-fl-l">👋 Hello, moi c’est Thibaut</p>
                         <h1 className="font-paytoneOne text-t-fl-3xl">Développeur web front-end</h1>
