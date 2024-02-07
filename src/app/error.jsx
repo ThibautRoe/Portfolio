7ca766aea98ae043d67d90939f39076d7a1a5420
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2024-02-07 23:37:17 +0100

    Fix on error and not-found pages to remove splashscreen

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -11,6 +11,18 @@ export default function Error({ error, reset }) {
         console.error(error)
     }, [error])
 
+    useEffect(() => {
+        const removeSplashScreen = () => {
+            const loader = document.getElementById("splashScreen")
+            if (loader) {
+                document.body.style.overflow = "auto"
+                loader.remove()
+            }
+        }
+
+        removeSplashScreen()
+    }, [])
+
     return (
         <div className="w-full h-full flex flex-col gap-s-fl-xl items-center justify-center bg-gradient-to-b from-custom-400 to-custom-300 dark:from-neutral-900 dark:to-neutral-800">
             <p className="text-t-fl-4xl">Un problème est survenu</p>
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,22 @@
+"use client"
+
+import { useEffect } from "react"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
 import AnimatedButton from "@/components/AnimatedButton"
 
 export default function NotFound() {
+    useEffect(() => {
+        const removeSplashScreen = () => {
+            const loader = document.getElementById("splashScreen")
+            if (loader) {
+                document.body.style.overflow = "auto"
+                loader.remove()
+            }
+        }
+
+        removeSplashScreen()
+    }, [])
+
     return (
         <div className="w-full h-full flex flex-col gap-s-fl-xl items-center justify-center bg-gradient-to-b from-custom-400 to-custom-300 dark:from-neutral-900 dark:to-neutral-800 ">
             <p className="text-t-fl-4xl">Perdu ?</p>
