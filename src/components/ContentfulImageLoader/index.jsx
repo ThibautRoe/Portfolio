67511393b8de6299e2b9e4725fe747a660ef9289
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2024-02-22 18:38:30 +0100

    Fix in custom image loader component (one more "&" character than needed)

--- src/components/ContentfulImageLoader/index.jsx
+++ src/components/ContentfulImageLoader/index.jsx
@@ -1,3 +1,3 @@
 export default function ContentfulImageLoader({ src, width, quality }) {
-    return `${src}?${width ? `w=${width}&` : ""}&q=${quality || 75}`
+    return `${src}?${width ? `w=${width}&` : ""}q=${quality || 75}`
 }
