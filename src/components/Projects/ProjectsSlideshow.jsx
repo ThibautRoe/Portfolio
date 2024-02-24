0a52b62cb42dba2810ca34f0ae017ecf7508e4b5
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2024-02-24 22:53:21 +0100

    Fix on projects slideshow in order to workaround a bug on Safari desktop with CSS translate3d rule

--- src/components/Projects/ProjectsSlideshow.jsx
+++ src/components/Projects/ProjectsSlideshow.jsx
@@ -11,15 +11,23 @@ import { register } from "swiper/element/bundle"
 register()
 
 export default function ProjectsSlideshow({ projects }) {
+    const projectsSwiperSelector = ".projects-swiper"
+    const slideSelector = ".project-slide"
+    const activeSlideSelector = ".swiper-slide-active"
     const reduceMotion = useReduceMotion()
     const preload = "auto"
-    const projectsSwiperSelector = ".projects-swiper"
-    const activeVideoSelector = ".projects-swiper .swiper-slide-active video"
 
     const { ref, inView, entry } = useInView({
         threshold: 0.5,
         delay: 1500,
     })
+
+    function findActiveSlideVideo() {
+        const activeSlideShadowDom = document.querySelector(projectsSwiperSelector).shadowRoot.querySelector(activeSlideSelector).part[1]
+        const activeVideoLightDom = document.querySelector(`${slideSelector}[slot='${activeSlideShadowDom}'] video`)
+
+        return activeVideoLightDom
+    }
 
     function findPlayButton(video) {
         let playButton = video.nextElementSibling
@@ -49,7 +57,7 @@ export default function ProjectsSlideshow({ projects }) {
 
     function handleVideosOnSlideChange() {
         const projectsVideos = document.querySelectorAll(`${projectsSwiperSelector} video`)
-        const activeSlideVideo = document.querySelector(activeVideoSelector)
+        const activeSlideVideo = findActiveSlideVideo()
 
         projectsVideos.forEach((video) => {
             if (video !== activeSlideVideo) {
@@ -72,7 +80,7 @@ export default function ProjectsSlideshow({ projects }) {
     }
 
     function toggleVideo() {
-        const activeSlideVideo = document.querySelector(activeVideoSelector)
+        const activeSlideVideo = findActiveSlideVideo()
         const doNoReset = true
 
         if (activeSlideVideo) {
@@ -93,6 +101,9 @@ export default function ProjectsSlideshow({ projects }) {
             projectsVideos.forEach((video) => {
                 video.addEventListener("contextmenu", handleContextMenu)
                 video.addEventListener("ended", handleVideoEnded)
+                video.addEventListener("suspend", () => {
+                    console.log("couscous") //TODO
+                })
             })
         }
 
@@ -102,6 +113,7 @@ export default function ProjectsSlideshow({ projects }) {
                 projectsVideos.forEach((video) => {
                     video.removeEventListener("contextmenu", handleContextMenu)
                     video.removeEventListener("ended", handleVideoEnded)
+                    video.removeEventListener("suspend", handleVideoEnded) // TODO
                 })
             }
         }
@@ -109,8 +121,7 @@ export default function ProjectsSlideshow({ projects }) {
 
     useEffect(() => {
         const projectsVideos = document.querySelectorAll(`${projectsSwiperSelector} video`)
-        const activeSlideVideo = document.querySelector(activeVideoSelector)
-        console.log(reduceMotion)
+        const activeSlideVideo = findActiveSlideVideo()
 
         if (!inView && projectsVideos) {
             projectsVideos.forEach((video) => pauseVideo(video, findPlayButton(video)))
@@ -136,10 +147,14 @@ export default function ProjectsSlideshow({ projects }) {
             pagination="true"
             pagination-clickable="true"
         >
-            {projects.map((item) => (
-                <swiper-slide
+            {projects.map((item, index) => (
+                <div
+                    // <div slot="slide-x"> instead of <swiper-slide> to prevent a bug in Safari desktop: https://swiperjs.com/blog/slide-slots-in-v10-1-0
+                    // Works great on Safari but it has downsides: the <swiper-slide> (with classes like "swiper-slide-active") are in the shadow DOM and <video> are in the light DOM so it's less easy than before to identify which video is on the active slide
+                    // See this commit for code before this changes: https://github.com/ThibautRoe/Portfolio/commit/4341c3641906e2de53f53d262ab9e33782970ca4
                     key={`slide-${item.id}`}
-                    /* lazy="true" */ class="self-center flex justify-center items-center drop-shadow-lg"
+                    slot={`slide-${index}`}
+                    /* lazy="true" */ className="project-slide self-center flex justify-center items-center drop-shadow-lg"
                 >
                     <ProjectCard
                         actionOnClick={toggleVideo}
@@ -156,9 +171,13 @@ export default function ProjectsSlideshow({ projects }) {
                         figma={item.figma}
                         livePreview={item.livePreview}
                     />
-                </swiper-slide>
+                </div>
             ))}
-            <swiper-slide key={`slide-malt-profile`} class="self-center flex justify-center items-center drop-shadow-lg">
+            <div
+                key={`slide-malt-profile`}
+                slot={`slide-${projects.length}`}
+                className="project-slide self-center flex justify-center items-center drop-shadow-lg"
+            >
                 <div className="rounded-s-fl-s flex flex-grow justify-center items-center aspect-[1.5/1] w-full min-h-[285px] max-h-[75dvh] sm:max-w-[95%] md:max-w-[90%] lg:max-w-[85%] xl:max-w-[80%] 2xl:max-w-[75%] color-transition bg-gradient-to-tl from-white/0 to-white/30 bg-neutral-500 dark:bg-neutral-700 mb-s-fl-l">
                     <AnimatedButton
                         link="" /* TODO Mettre le lien vers mon profil */
@@ -171,7 +190,7 @@ export default function ProjectsSlideshow({ projects }) {
                         }
                     />
                 </div>
-            </swiper-slide>
+            </div>
         </swiper-container>
     )
 }
