a0b45aba0f9bdf8b39aaf85294b6aeea0194ccbf
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2024-02-25 18:02:08 +0100

    Preload lottie animation file in <head>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,6 +27,10 @@ export default function RootLayout({ children }) {
             // overflow-hidden on html + 100dvh on body allows for the browser UI on mobile to never hide (html never scrolls because its only child body is always full height and no more), which helps with UX on mobile when scrolling when snap-mandatory is on and each section height is set to 100dvh (dvh → which means the section height changes when the browser UI hides / unhides)
             // The downside is that it breaks scroll position restoration on all browsers but Firefox. Scroll restoration on anchors links (http://thibautroegiers.dev/#skills for example) on page refresh still works on Firefox and Safari but not on Chrome based browsers (Chrome, Opera, Arc, etc.).
         >
+            <head>
+                <link rel="preload" as="fetch" crossorigin="anonymous" type="application/zip+lottie" href="/hero-animation.lottie" />
+                {/* Can't find a way to easily preload assets with Next.js app router without using <head> (not supposed to use it like that) */}
+            </head>
             {/* TODO Can't find a way for SplashScreen to get darkmode right on first render, so for now default to dark mode on first load and dark mode applied by HeroAnimation component when removing SplashScreen when animation starts*/}
             <body className="h-dvh w-dvw scroll-smooth overflow-x-hidden overflow-y-auto font-nunito text-t-fl-base text-neutral-600 dark:text-neutral-50">
                 {/* <DynamicTestViewport /> */}
