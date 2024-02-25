d856b49e61695c9c13c0f8c871caa72f18430913
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2024-02-25 13:48:23 +0100

    Fix on color transition on AnimationButton + change to AnimatedMouse border width

--- src/components/Hero/AnimatedMouse/index.jsx
+++ src/components/Hero/AnimatedMouse/index.jsx
@@ -6,7 +6,7 @@ export default function AnimatedMouse() {
         <Link
             href="#about"
             aria-label="Aller à la section suivante"
-            className="color-transition animated-mouse relative aspect-[9/16] w-s-fl-m rounded-s-fl-xs border-[0.08em] border-neutral-600 dark:border-neutral-50"
+            className="color-transition animated-mouse relative aspect-[9/16] w-s-fl-m rounded-s-fl-xs border-[0.09em] border-neutral-600 dark:border-neutral-50"
         >
             <div className="color-transition animated-mouse-pointer absolute left-1/2 top-[70%] -translate-x-1/2 aspect-square w-s-fl-3xs rounded-full bg-neutral-600 dark:bg-neutral-50"></div>
         </Link>
