bb3378feca745dc9717eada47c79fed1c39a38f1
Thibaut Roegiers <137530922+ThibautRoe@users.noreply.github.com>
2024-06-03 17:18:57 +0200

    Fix for 1px height header background color on mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,27 +6,32 @@ import IconMail from "@/components/ui/icons/src/animated-on-render/icon-mail"
 
 export default function Header() {
     return (
-        <header
-            id="home"
-            className="nav-anchor color-transition lg:bg-custom-400 dark:lg:bg-neutral-800 text-neutral-600 dark:text-neutral-400 lg:text-neutral-600 dark:lg:text-neutral-50 snap-start h-[1px] lg:h-auto"
-        >
-            {/* 1px height plutôt que hidden sur mobile sinon le snap-start n'est pas pris en compte donc on le peut plus scroller sur cette section et le #home hash link ne fonctionne plus  */}
-            <div className="lg:u-container">
-                <div className="flex">
-                    <AnimatedText once text="<Thibaut />" el="p" className="hidden lg:inline text-t-fl-2xl" />
-                    <Navigation header={true} />
-                    <AnimatedButton
-                        link="mailto:hello@thibautroegiers.dev"
-                        linkClass="hidden lg:flex"
-                        text="Contactez-moi"
-                        iconBefore={
-                            <BaseIcon width="1.2em" height="1.2em" viewBox="0 0 24 24">
-                                <IconMail />
-                            </BaseIcon>
-                        }
-                    />
-                </div>
-            </div>
-        </header>
+      <header
+        id="home"
+        className="nav-anchor color-transition bg-custom-400 dark:bg-neutral-800 text-neutral-600 dark:text-neutral-400 lg:text-neutral-600 dark:lg:text-neutral-50 snap-start h-[1px] lg:h-auto"
+      >
+        {/* 1px height plutôt que hidden sur mobile sinon le snap-start n'est pas pris en compte donc on le peut plus scroller sur cette section et le #home hash link ne fonctionne plus  */}
+        <div className="lg:u-container">
+          <div className="flex">
+            <AnimatedText
+              once
+              text="<Thibaut />"
+              el="p"
+              className="hidden lg:inline text-t-fl-2xl"
+            />
+            <Navigation header={true} />
+            <AnimatedButton
+              link="mailto:hello@thibautroegiers.dev"
+              linkClass="hidden lg:flex"
+              text="Contactez-moi"
+              iconBefore={
+                <BaseIcon width="1.2em" height="1.2em" viewBox="0 0 24 24">
+                  <IconMail />
+                </BaseIcon>
+              }
+            />
+          </div>
+        </div>
+      </header>
     )
 }
