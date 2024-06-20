8a59bc58779c6dc63f42ba3d0eb80a39f8770916
Thibaut Roegiers <137530922+ThibautRoe@users.noreply.github.com>
2024-06-20 18:14:31 +0200

    Updated opengraph and twitter image + fix in a comment in getCurrentAge() fuction + packages updated in previous commit

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,19 +6,19 @@ export function cn(...inputs) {
 }
 
 export function getCurrentAge() {
-    const dateToday = new Date()
-    const dateOfBirth = new Date(Date.UTC(1989, 5, 21, 22, 0, 0))
-    // 5 et pas 6 pour Juin car c'est le monthIndex (Janvier = 0) Ici c'est le 21/06/1989 à 22h00 UTC soit le 22/06/1989 à 0h00 GMT+2
-    let age = dateToday.getFullYear() - dateOfBirth.getFullYear()
-    const monthDiff = dateToday.getMonth() - dateOfBirth.getMonth()
+  const dateToday = new Date()
+  const dateOfBirth = new Date(Date.UTC(1989, 5, 21, 22, 0, 0))
+  // 5 et pas 6 pour Juin car c'est le monthIndex (Janvier = 0) Ici c'est le 21/06/1989 à 22h00 UTC soit le 22/06/1989 à 0h00 UTC+2
+  let age = dateToday.getFullYear() - dateOfBirth.getFullYear()
+  const monthDiff = dateToday.getMonth() - dateOfBirth.getMonth()
 
-    if (monthDiff < 0) {
-      age--
-    } else if (monthDiff === 0 && dateToday.getDate() < dateOfBirth.getDate()) {
-      age--
-    }
+  if (monthDiff < 0) {
+    age--
+  } else if (monthDiff === 0 && dateToday.getDate() < dateOfBirth.getDate()) {
+    age--
+  }
 
-    return age
+  return age
 }
 
 export function getCurrentYear() {
